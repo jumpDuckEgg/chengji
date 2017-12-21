be947e5f38e7a7bf2ce03b31fc22af7b16f511af
jumpDuck <1197369402@qq.com>
2017-12-21 15:56:35 +0800

    完成一部分

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cssnano = require('gulp-cssnano');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+//编译html
+gulp.task('html', function(){
+	return gulp.src('index.html')
+		.pipe(plumber({
+			errorHandler: notify.onError('Error: <%= error.message %>')
+		}))
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.stream());
+});
+//编译resources
+gulp.task('resource', function(){
+	return gulp.src('./resource/**/*')
+		.pipe(plumber({
+			errorHandler: notify.onError('Error: <%= error.message %>')
+		}))
+		.pipe(gulp.dest('./app/resource/'))
+		.pipe(browserSync.stream());
+});
+//编译css
+gulp.task('css', function(){
+	return gulp.src('css/main.css')
+		.pipe(plumber({
+			errorHandler: notify.onError('Error: <%= error.message %>')
+		}))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
+});
+
+//编译js
+gulp.task('script', function(){
+	return gulp.src('./js/**/*.js')
+		.pipe(plumber({
+			errorHandler: notify.onError('Error: <%= error.message %>')
+		}))
+		.pipe(gulp.dest('./app/js'))
+		.pipe(browserSync.stream());
+});
+//监测变化
+gulp.task('browser-sync', ['html', 'resource', 'css', 'script'], function(){
+	browserSync.init({
+		server: {
+            baseDir: "app/"
+        }
+	});
+	//监听文件的变化
+	gulp.watch("index.html", ['html']);
+	gulp.watch("resource/**/*", ['resources']);
+	gulp.watch("css/*.css", ['css']);
+	gulp.watch("js/*.js", ['script']);
+});
+gulp.task('default',['browser-sync'])
--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
-$(function(){
-    //初始化车票列表高度
-    $('.ticket-list').css('height',$('body').height()-($('.remain').height()+$('.footer-bar').height()+$('.choose-ticket').height()));
-    
+$(function () {
+    //初始化
+    init();
+    // 选择按钮切换样式
+    buttonEvent();
 });
+function init() {
+ 
+    //初始化车票列表高度
+    $('.ticket-list').css('height', $('body').height() - ($('.remain').height() + $('.footer-bar').height() + $('.choose-ticket').height()));
+    $('.time-place_up span,.time-place_down span').css('width', $('.time-place_up').width() - $('.time-place_up img').width() - 5);
+    //初始化开始地点和结束地点大小
+    $('.start-place').css('width', $('.start').width() - $('.start img').width() - $('.start price').width() - 80);
+    $('.end-place').css('width', $('.end').width() - $('.end img').width() - 5);
+}
+function buttonEvent() {
+    var $onceTicket = $('.OnceTicket');
+    var $monthTicket = $('.MonthTicket');
+    var $onceTicketList = $('.onceTicket-list');
+    var $monthTicketList = $('.monthTicket-list');
+    $onceTicket.click(function () {
+        if (!$onceTicket.hasClass('choose-color')) {
+            $onceTicket.addClass('choose-color');
+            $monthTicket.removeClass('choose-color');
+            $onceTicketList.css('display', 'block').scrollTop(0);
+            $monthTicketList.css('display', 'none');
+
+        }
+    })
+    $monthTicket.click(function () {
+        if (!$monthTicket.hasClass('choose-color')) {
+            $monthTicket.addClass('choose-color');
+            $onceTicket.removeClass('choose-color');
+            $monthTicketList.css('display', 'block').scrollTop(0);
+            $onceTicketList.css('display', 'none');
+        }
+    })
+}
+
+
