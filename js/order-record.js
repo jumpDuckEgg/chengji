9f38a059b2e488e142237c94f83539e0adc216a0
jumpDuck <1197369402@qq.com>
2017-12-27 17:07:34 +0800

    12.27

--- js/order-record.js
+++ js/order-record.js
@@ -1,43 +1,32 @@
 $(function () {
-    //初始化
     init();
-    
-    //切换使用中已使用标签样式
     ticketTab();
 });
-function init() {
-
-    //初始化车票列表高度
-    $('.ticket-list').css('height', $('body').height() - ($('.ticketused-tab').height() + $('.headbar').height()));
-    $('.ticket-information:last-child').css('margin-bottom', $('.footer-bar').height()+parseInt($('.ticket-information:first-child').css('margin-top')) );
-
-}
-
 function ticketTab(){
     var $itemleft=$('.item-left');
     var $itemright=$('.item-right');
-    var $usedlist=$('.used');
-    var $finishlist=$('.finish');
-    var $footer=$('.footer-bar');
+    var $ticket_information=$('.ticket-information');
+    var $ticket_information_fail=$('.ticket-information_fail');
     $itemleft.click(function () {
         if (!$itemleft.hasClass('item-choose')) {
-            $itemleft.addClass('item-choose');
-            $itemright.removeClass('item-choose');
-            $usedlist.css('display', 'block').scrollTop(0);
-            $finishlist.css('display', 'none');
-            $footer.css('display', 'block');    
+            $itemleft.addClass('item-choose ticketused-tab_content-item_choose');
+            $itemright.removeClass('item-choose ticketused-tab_content-item_choose');  
+            $ticket_information.parent().css('display','block').scrollTop(0);
+            $ticket_information_fail.parent().css('display','none');    
         }
     })
     $itemright.click(function () {
         if (!$itemright.hasClass('item-choose')) {
-            $itemright.addClass('item-choose');
-            $itemleft.removeClass('item-choose');
-            $finishlist.css('display', 'block').scrollTop(0);
-            $usedlist.css('display', 'none');  
-            $footer.css('display', 'none');
-                 
+            $itemright.addClass('item-choose ticketused-tab_content-item_choose');
+            $itemleft.removeClass('item-choose ticketused-tab_content-item_choose');
+            $ticket_information_fail.parent().css('display','block').scrollTop(0);
+            $ticket_information.parent().css('display','none');   
         }
     })
 }
+function init(){
+    $('.ticket-list').css('height',$('body').height()-$('.headbar').height()-$('.ticketused-tab').height());
+    console.log($('body').height(),$('.headbar').height(),$('.ticketused-tab').height(),$('.ticket-list').height());
 
+}
 
